699f985ff913ec3b6014b8e43d4262d73cf4a874
Lucas Carvalho <lucascarvalhoo@outlook.com>
2026-01-31 21:20:32 -0300

    Botão de deletar conta com confirmação

--- src/main/java/ui/TelaPrincipal.java
+++ src/main/java/ui/TelaPrincipal.java
@@ -6,17 +6,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import service.metodos;
 import service.metodos.Usuario;
 import service.metodos.Usuario;
 import java.awt.*;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class TelaPrincipal {
     @FXML
     private Label mensagem;
+    private void alert(String mensagem) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText(mensagem);
+        alert.showAndWait();
+    }
     @FXML
     private void abrirLogin(ActionEvent event){
         try {
@@ -46,5 +56,22 @@ public class TelaPrincipal {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    @FXML
+    private void apagarConta(ActionEvent event){
+        Usuario u = Sessao.usuarioLogado;
+        Alert aviso = new Alert(Alert.AlertType.CONFIRMATION);
+        aviso.setContentText("Tem certeza? isso apagara sua conta para sempre");
+        Optional<ButtonType> confirmar = aviso.showAndWait();
+        if (confirmar.get() == ButtonType.OK){
+            try {
+                metodos.deletar_conta(u.getEmail());
+                alert("Usuario deletado com sucesso! voltando a tela de login");
+                abrirLogin(event);
+            }catch (SQLException ex){
+                alert("Falha ao deletar usuario " + ex.getMessage());
+            }
+        }
     }
 }
